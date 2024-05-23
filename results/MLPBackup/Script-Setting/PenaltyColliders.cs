e48fa93789909e2a933212fa886efa9c83652bbd
guanyulin28193 <garry.lin98@gmail.com>
2024-05-23 08:11:55 +0200

    mod

---
+++ results/MLPBackup/Script-Setting/PenaltyColliders.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenaltyColliders : MonoBehaviour
+{
+    public LowLvlAgent agent;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Debug.Log("Penalty: " + gameObject.name + " collided with " + other.gameObject.name);
+        //&& other.gameObject.name == "Peg"
+        if ((gameObject.name == "FingerA" || gameObject.name == "FingerB") )
+        {
+            agent.PegHitPenalty(other.gameObject);
+        }
+        else
+        {   
+            agent.GroundHitPenalty();
+        }
+        
+    }
+}
